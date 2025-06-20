229222a84cf102dfc7217ca700a6b7737de5dbea
ÿago <yagomartins30@gmail.com>
2025-06-20 12:09:12 -0300

    feito a pastas do lab csrf

---
+++ labs/csrf_attack/csrf_attack.py
@@ -0,0 +1,9 @@
+import sys, os, datetime, jwt, json, base64
+from flask import render_template, request, make_response, jsonify, Flask
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from getIP import get_local_ip, get_port
+
+ip = get_local_ip()
+port = get_port()
+ROOT_INDEX = f'http://{ip}:{port}'
+
